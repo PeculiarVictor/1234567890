8eab4a33bef338aea3553b88f61fe6194345bef0
PeculiarVictor <160975802+PeculiarVictor@users.noreply.github.com>
2025-01-31 18:56:30 +0000

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,8 @@
+string = input("please enter you8r own string : word")
+
+
+string2 = ("")
+for i in string:
+     string2 = i + string2
+print("\nThe Original string =" , string)
+print("\nThe Reversed string =" , string2)
